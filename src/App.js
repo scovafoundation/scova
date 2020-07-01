6561b5d8f87a6051d558b04832124682b85388b5
Anhad Arora <anhad@Anhads-MacBook-Pro.local>
2020-07-01 19:04:31 +0530

    added prefaces, and font changes, and images

--- src/App.js
+++ src/App.js
@@ -13,15 +13,24 @@ import {
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import verses from './Scova'
-
+import {verses, onboarding } from './Scova';
 
 function App() {
   const versesNumbered = verses.map((verse, index) => 
   <tr>
-    <td colSpan="8" style={{fontWeight: "bolder", textAlign:"left"}}>{index+1}</td>
-    <td colSpan="42" className={verse.classes}>{verse.html}</td>
+    <td colSpan="8" style={{fontWeight: "bolder", textAlign:"left"}}>
+      {index+1}
+    </td>
+    <td colSpan="42" className={verse.classes} id={index+1}>
+      {verse.html}
+    </td>
     </tr>
+  );
+
+  const onboardingFormatted = onboarding.map((verse, index) => 
+  <p className={verse.classes}>
+    <br/><br/>{verse.html}<br/>
+  </p>
   );
 
 
@@ -29,6 +38,8 @@ function App() {
     <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
 
+
+        {onboardingFormatted}
         <table>
           {versesNumbered}
         </table>
